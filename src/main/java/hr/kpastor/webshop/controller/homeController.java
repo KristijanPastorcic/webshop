e9b5d86c79074c0b1fa12b82c0de026546318165
Kristijan Pastorčić <kpastorcic11@gmail.com>
2023-06-03 17:50:38 +0200

    layout and home page

---
+++ src/main/java/hr/kpastor/webshop/controller/homeController.java
@@ -0,0 +1,16 @@
+package hr.kpastor.webshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class homeController {
+
+    @GetMapping
+    public String plp() {
+        return "views/home";
+    }
+
+}
