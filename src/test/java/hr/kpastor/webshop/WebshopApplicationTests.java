9399dd4bf85323f28b90facfba0f0a50223b32a3
Kristijan Pastorčić <kpastorcic11@gmail.com>
2023-06-03 17:49:22 +0200

    initial commit

---
+++ src/test/java/hr/kpastor/webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.kpastor.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
