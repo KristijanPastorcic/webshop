e0eb0427edebf41f94cd808cea67f52c65ea3439
kpast <kpastorcic11@gmail.com>
2023-08-28 10:52:49 +0200

    commit

--- src/main/java/hr/kpastor/webshop/WebshopApplication.java
+++ src/main/java/hr/kpastor/webshop/WebshopApplication.java
@@ -19,31 +19,4 @@ public class WebshopApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebshopApplication.class, args);
     }
-
-    // test db connection
-    private static void testDBConnection() {
-
-        // Replace the placeholder with your Atlas connection string
-        String uri = System.getenv("MONGODB_CONNECTION_STRING");
-        // Construct a ServerApi instance using the ServerApi.builder() method
-        ServerApi serverApi = ServerApi.builder()
-                .version(ServerApiVersion.V1)
-                .build();
-        MongoClientSettings settings = MongoClientSettings.builder()
-                .applyConnectionString(new ConnectionString(uri))
-                .serverApi(serverApi)
-                .build();
-        // Create a new client and connect to the server
-        try (MongoClient mongoClient = MongoClients.create(settings)) {
-            MongoDatabase database = mongoClient.getDatabase("admin");
-            try {
-                // Send a ping to confirm a successful connection
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                Document commandResult = database.runCommand(command);
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-            } catch (MongoException me) {
-                System.err.println(me);
-            }
-        }
-    }
 }
